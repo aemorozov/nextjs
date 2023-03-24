995655297d6885025aac60866c8460e05236d643
aemorozov <mclenina19@gmail.com>
2023-03-24 14:05:26 +0400

    chore: add styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Heading from "@/components/Heading"
+import styles from '../styles/Home.module.scss'
 
 const Home = () => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Heading tag="h1" text="Hello World!" />
-    </>
+    </div>
   )
 }
 
