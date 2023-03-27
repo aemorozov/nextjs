6b79b8e2d41781eafd95ce261a738cc352cae2dc
aemorozov <mclenina19@gmail.com>
2023-03-27 16:36:37 +0400

    chore: users layout on server from API

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -6,6 +6,12 @@ export const getStaticProps = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users')
   const data = await response.json()
 
+  if(!data) {
+    return {
+      notFound: true
+    }
+  }
+
   return {
     props: { contacts: data }
   }
